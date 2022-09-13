7bc47935a95a69090b654fdf29ccb1b88324740b
xansiety <ferando543@outlook.com>
2022-09-12 19:20:54 -0500

    Deteccion y mensajes de conexion y de desconexion - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,6 +1,17 @@
- const socket = io()
+const socket = io();
 
+//referencias del html
+const lblOnline  = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
 
+socket.on("connect", () => {
+    console.log('Conectado');
+    lblOffline.style.display = 'none'
+    lblOnline.style.display = ''
+});
 
-console.log('Hola mundo');
-
+socket.on('disconnect', () => {
+    console.log('Desconectado');
+    lblOnline.style.display = 'none'
+    lblOffline.style.display = ''
+});
